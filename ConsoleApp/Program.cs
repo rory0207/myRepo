6495ad58fd768dbd447137d43987f79d8de6bec5
hsieh,rory <rory0207@outlook.com>
2015-09-22 16:55:20 +0800

    list matrixA

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,11 +11,34 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            int[,] arrayA = new int[5,5] { { 0, 2, 0, 0, 0 }, { 1, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 1 }, { 0, 1, 1, 0, 0 } };
+            //int[,] arrayA = new int[5,5] { { 0, 2, 0, 0, 0 }, { 1, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 1 }, { 0, 1, 1, 0, 0 } };
 
-            //int[,] arrayA = new int[2, 3] { { 2, 3 ,3}, { 1, 5, 4 }};
+            int[,] matrixA = new int[2, 3] { { 1, 1 ,2}, { 1, 2, 1 }};
+
+            int[,] matrixB = new int[3, 2] { { 3, 1 }, { 1, 2 }, { 3, 1 } };
 
             //int[,] arrayA = new int[3, 2] { { 2,4}, { 3,2} , { 0,1} };
+
+
+            for (int i=0; i< matrixA.GetLength(0); i++)
+            {
+
+                for (int j=0;j< matrixA.GetLength(1); j++)
+                {
+                    //Console.Write("matrixA[{0},{1}]: {2}  ", i, j, matrixA[i, j]);
+
+                    Console.Write("{0}  ", matrixA[i, j]);
+
+                    //Console.Write("j+1 = {0} % matrixA.GetLength(1) = {1} = {2}", j + 1, matrixA.GetLength(1), (j + 1) % matrixA.GetLength(1) );
+                    
+                    if ((j+1) % matrixA.GetLength(1) == 0)
+                    {
+                        // Redefine the newline characters to double space.
+                        Console.Write("\r\n\r\n");
+                    }
+                }                
+            }
+
 
 
             //// show element 1
@@ -25,23 +48,30 @@ namespace ConsoleApp
             //}
 
 
-            // show element 3
-            int count = 0;
-            foreach (int element in arrayA)
-            {
-                count += 1;
-                
-                Console.Write("#{0}:{1} ", count, element);
+            // show element 2
+            //int count = 0;
+            //foreach (int element in matrixA)
+            //{
+            //    count += 1;
 
-                if (count % arrayA.GetLength(1) == 0)
-                {
-                    // Redefine the newline characters to double space.
-                    Console.Write("\r\n\r\n");
-                }
-            }
+            //    // Console.Write("#{0}:{1} ", ("00"+ count).PadRight(2), ("00"+element).PadRight(2));
+            //    Console.Write("{0} ", ("0" + element).PadRight(2));
 
-            Console.Write("\r\n\r\n");
-            Console.WriteLine("Number of elements in the array: {0}", count);
+
+
+            //    if (count % matrixA.GetLength(1) == 0)
+            //    {
+            //        // Redefine the newline characters to double space.
+            //        Console.Write("\r\n\r\n");
+            //    }
+            //}
+
+            //Console.Write("\r\n\r\n");
+            //Console.WriteLine("Number of elements in the array: {0}", count);
+
+
+
+
 
             Console.ReadLine();
 
